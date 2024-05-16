3b3a4b74f6051becfe121a6b15949977ec51e6e2
Joyosmit <joyosmitp@gmail.com>
2024-05-16 21:31:32 +0530

    Added functionality to get all conversations in left-side panel

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,6 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
-import { group } from "console";
+import { mutation, query } from "./_generated/server";
 
 export const createConversation = mutation({
     args: {
@@ -50,6 +49,63 @@ export const createConversation = mutation({
         return conversationId;
     }
 })
+
+// get all of my conversations from the database
+export const getMyConversations = query({
+    args: {},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if(!identity) {
+            throw new ConvexError("Unauthorized");
+        }
+
+        const user = await ctx.db
+        .query("users")
+        .withIndex("by_tokenIdentifier", q => q.eq("tokenIdentifier", identity.tokenIdentifier))
+        .unique()
+
+        if(!user) throw new ConvexError("User not found");
+
+        // get all convos
+        const conversations = await ctx.db.query("conversations").collect();
+
+        // get only MY convos
+        const myConversations = conversations.filter((conversation) => {
+            return conversation.participants.includes(user._id)
+        })
+
+        // we now need other details of the participants
+        const conversationsWithDetails = await Promise.all(
+            myConversations.map(async (conversation) => {
+                let userDetails = {};
+                if(!conversation.isGroup){
+                    const otherUser = conversation.participants.find((id) => id !== user._id);
+                    const userProfile = await ctx.db
+                    .query("users")
+                    .filter(q => q.eq(q.field("_id"), otherUser))
+                    .take(1)
+
+                    userDetails = userProfile[0];
+                }
+
+                const lastMessage = await ctx.db
+                .query("messages")
+                .filter(q => q.eq(q.field("conversation"), conversation._id))
+                .order("desc")
+                .take(1)
+
+                return {
+                    ...userDetails,
+                    ...conversation,
+                    lastMessage: lastMessage[0] || null,
+                }
+            })
+        )
+
+        return conversationsWithDetails;
+    }
+})
+
 
 export const generateUploadUrl = mutation(async(ctx) => {
     return await ctx.storage.generateUploadUrl()
